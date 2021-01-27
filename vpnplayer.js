3ecc603b965c3458898d26fbc42fa106da8204b3
WikiBotFoundation <70801867+WikiBotFoundation@users.noreply.github.com>
2021-01-27 16:44:22 +0300

    New js

---
+++ vpnplayer.js
@@ -0,0 +1,20 @@
+// Script Loaded //
+   
+data(script="loader.vpn(ipViewer.LicenseKey:000, 
+    localhost(vpn).script(ipadresses.port
+          LocalVPN(developtool.port
+       VPN.player(html.script(java.import(data)
+               Localhost;000.data(scriptVPN
+    Localhost="css.player(script.body(import(data)
+          port(local.vpnplayer.script(java)
+          88.ip(world data(localhost:000
+  Https://,data(https,Server.vpn
+           88.80.0.0(port.server(randomserver.88.0.0.0
+
+// p
+     
+
+
+csplayerlocation.(css.country(united.states(fakeDetails)
+             Javascript.data(script.computervpn.ip(script.extractor.data
+     Javascript.data(scriptcommand.computervpn.turkey.teleportvpn
