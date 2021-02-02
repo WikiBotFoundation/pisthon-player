5d97d5ae631730c5ce1817bb9fc06d5c9ac24549
WikiBotFoundation <70801867+WikiBotFoundation@users.noreply.github.com>
2021-02-02 14:34:37 +0300

    WikiLanugaue

---
+++ WikiLanguage.php
@@ -0,0 +1,20 @@
+# localhost
+# script is loading
+# copying script
+      Php 
+    confrim="key-api(script)"
+         @$SetupKey="html(e.local)"
+         @$DomainApi="domain.(e.local, api)
+            
+    confirm="key-token == bot.e(local)"
+            confirm="key.load(domain.php)"
+      serverTeleporter="ip.key(0.0.0.0)
+         @$ConfirmDomain="z(script.local
+       88.2010.010 = "ipteleporter"
+       @$web="permission.teleport
+                   88.0.0.010.02 = api.key.load
+       webdomain:permission 
+                php.data="load"
+
+
+       
